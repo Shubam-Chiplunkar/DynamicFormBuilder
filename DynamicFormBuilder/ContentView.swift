11f675b1d47631d9e382ed3ae647d131a4c9c2f4
Shubham Chiplunkar <shubham.chiplunkar21@gmail.com>
2026-06-12 00:06:04 +0530

    Initial Commit

---
+++ DynamicFormBuilder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DynamicFormBuilder
+//
+//  Created by Shubham Chiplunkar on 12/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DynamicFormBuilder/DynamicFormBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicFormBuilderApp.swift
+//  DynamicFormBuilder
+//
+//  Created by Shubham Chiplunkar on 12/06/26.
+//
+
+import SwiftUI
+
+@main
+struct DynamicFormBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
